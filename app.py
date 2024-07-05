e6640e298e7d0ed6016e9b1c57abc59cf08dee15
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-07-05 15:48:10 +0400

    final commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+import numpy as np
 
 model = tf.keras.models.load_model('/Users/jahezabrahamjohny/Documents/GitHub/CoreAxon/autoencoder_model.h5')
 
@@ -16,18 +17,32 @@ def hello_word():
 
 @app.route('/', methods=['POST'])
 def predict():
-    file= request.files['temprature data']
+    file= request.files['temperature_data']
     df = pd.read_csv(file)
+    train_size = int(len(df) * 0.8)  # 80% train, 20% test
+    #train_df = df.iloc[:train_size]
+    test_df = df.iloc[train_size:]
     temperatures = df['temperature'].tolist()
     anomaly_scores = model(temperatures)
-
-    # Generate plot
+    
+    # Plotting anomalies
     plt.figure(figsize=(10, 6))
-    plt.plot(temperatures, anomaly_scores, marker='o', linestyle='-', color='b')
-    plt.xlabel('Temperature')
-    plt.ylabel('Anomaly Score')
-    plt.title('Anomaly Detection')
+
+    plt.plot(test_df['timestamp'], test_df['temperature'], marker='o', linestyle='-', color='b', label='Temperature')
+
+# Highlight anomalies (threshold example)
+    threshold = np.mean(anomaly_scores) + 2 * np.std(anomaly_scores)
+    anomalies = test_df[anomaly_scores > threshold]
+    plt.scatter(anomalies['timestamp'], anomalies['temperature'], color='r', marker='x', label='Anomalies')
+
+    plt.xlabel('Timestamp')
+    plt.ylabel('Temperature')
+    plt.title('Temperature Anomaly Detection with Autoencoder')
+    plt.legend()
     plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    #plt.show()
             
     # Convert plot to base64 encoding and embed in HTML
     plot_img = BytesIO()
@@ -39,4 +54,4 @@ def predict():
     return render_template('index.html', plot_url=plot_url)
 
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(port=5000, debug=True)
